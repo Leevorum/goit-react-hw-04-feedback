8be8acec412803f204d36ea5d3a2084c964e0e8f
Leevorum <di4111ua@gmail.com>
2022-06-25 20:43:41 +0300

    feedback completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,67 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from 'components/Statistics/Statistics';
 import Section from 'components/Section/Section';
 import Notification from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleBtnIncrement = name => {
+    switch (name) {
+      case 'good':
+        setState({ ...state, good: state.good + 1 });
+        break;
+
+      case 'neutral':
+        setState({ ...state, neutral: state.neutral + 1 });
+        break;
+
+      case 'bad':
+        setState({ ...state, bad: state.bad + 1 });
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleBtnIncrement = name => {
-    this.setState(prevState => {
-      return {
-        [name]: prevState[name] + 1,
-      };
-    });
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((acc, feedback) => feedback + acc, 0);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce(
-      (acc, feedback) => feedback + acc,
-      0,
-    );
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback() === 0) {
+  const countPositiveFeedbackPercentage = () => {
+    if (countTotalFeedback() === 0) {
       return 0;
     }
-    const totalPositiveFeedback =
-      (this.state.good / this.countTotalFeedback()) * 100;
+    const totalPositiveFeedback = (state.good / countTotalFeedback()) * 100;
 
     return Math.round(totalPositiveFeedback);
   };
 
-  render() {
-    const totalFeedback = this.countPositiveFeedbackPercentage();
-    const stats = Object.entries(this.state);
-    const total = this.countTotalFeedback();
-    const options = Object.keys(this.state);
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbacks = countPositiveFeedbackPercentage();
+  const stats = Object.entries(state);
+  const options = Object.keys(state);
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleBtnIncrement}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleBtnIncrement}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {totalFeedback > 0 && (
+          <Statistics
+            stats={stats}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbacks}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {totalFeedback > 0 && (
-            <Statistics
-              stats={stats}
-              total={total}
-              positivePercentage={totalFeedback}
-            />
-          )}
-          {totalFeedback < 0 && <Notification message="There is no feedback" />}
-        </Section>
-      </div>
-    );
-  }
+        )}
+        {totalFeedback <= 0 && <Notification message="There is no feedback" />}
+      </Section>
+    </div>
+  );
 }
